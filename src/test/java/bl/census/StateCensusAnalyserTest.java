4057e793cf06f2f3d155e300e56105fef6f99c12
Prashant Deepake <deepakeprashant1@gmail.com>
2023-03-13 16:59:38 +0530

    Added: File Correct but Delimiter Incorrect Return EXception

--- src/test/java/bl/census/StateCensusAnalyserTest.java
+++ src/test/java/bl/census/StateCensusAnalyserTest.java
@@ -1,4 +1,5 @@
 package bl.census;
+
 import bl.csvfile.example.*;
 import bl.csvfile.example.CSVStateCensus;
 import bl.csvfile.example.CensusAnalyserException;
@@ -10,6 +11,7 @@ import java.io.IOException;
 
 public class StateCensusAnalyserTest {
     public static final String STATECENSUS_CSVFILE = "C:\\Users\\Admin\\IdeaProjects\\StateCensusAnalyser\\src\\main\\resources\\StateCensusData.csv";
+    public static final String STATECODE_CSVFILE = "C:\\Users\\Admin\\IdeaProjects\\StateCensusAnalyser\\src\\main\\resources\\StateCodeData.csv";
     public static final String WRONG_FILE = "/useless.txt";
 
     @Test
@@ -17,19 +19,39 @@ public class StateCensusAnalyserTest {
         try {
             int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILE, CSVStateCensus.class);
             System.out.println(count);
-            Assertions.assertEquals(4,count);
+            Assertions.assertEquals(4, count);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
         }
     }
 
     @Test
-    public void givenTheStateCensusCsvFile_IfIncorrect_ShouldReturnException() throws IOException{
+    public void givenTheStateCensusCsvFile_IfIncorrect_ShouldReturnException() throws IOException {
         try {
             int count = StateCensusAnalyser.openCsvBuilder(WRONG_FILE, CSVStateCensus.class);
-        }catch (CensusAnalyserException e){
+        } catch (CensusAnalyserException e) {
             e.printStackTrace();
-            Assertions.assertEquals(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE,e.type);
+            Assertions.assertEquals(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE, e.type);
+        }
+    }
+
+    @Test
+    public void GivenTheStateCensusCsvFile_WhenCorrect_ButFileExtensionIncorrect_ShouldThrowCensusAnalyserException() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(STATECODE_CSVFILE, CSVStateCensus.class);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assertions.assertEquals(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE, e.type);
+        }
+    }
+
+    @Test
+    public void GivenTheStateCensusCSVFile_WhenCorrect_ButDelimiterIncorrect_ReturnsCensusAnalyserException() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILE, CSVStateCensus.class);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+            Assertions.assertEquals(CensusAnalyserException.CensusExceptionType.DELIMITER_ISSUE, e.type);
         }
     }
 }

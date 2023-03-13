aeb3d78e8b0d3e88bbbbc42abcef9722f6f90c96
Prashant Deepake <deepakeprashant1@gmail.com>
2023-03-13 16:41:25 +0530

    Added: File Incorrect Return Custom Exception

--- src/test/java/bl/census/StateCensusAnalyserTest.java
+++ src/test/java/bl/census/StateCensusAnalyserTest.java
@@ -9,18 +9,27 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 public class StateCensusAnalyserTest {
-    public static final String STATECODES_CSVFILE = "/home/admin97/Ayush/github/MyCensusAnalyser/src/main/resources/StateCode.csv";
     public static final String STATECENSUS_CSVFILE = "C:\\Users\\Admin\\IdeaProjects\\StateCensusAnalyser\\src\\main\\resources\\StateCensusData.csv";
     public static final String WRONG_FILE = "/useless.txt";
 
     @Test
-    public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
+    public void givenTheStateCensusCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
         try {
             int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILE, CSVStateCensus.class);
             System.out.println(count);
             Assertions.assertEquals(4,count);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenTheStateCensusCsvFile_IfIncorrect_ShouldReturnException() throws IOException{
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(WRONG_FILE, CSVStateCensus.class);
+        }catch (CensusAnalyserException e){
+            e.printStackTrace();
+            Assertions.assertEquals(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE,e.type);
         }
     }
 }

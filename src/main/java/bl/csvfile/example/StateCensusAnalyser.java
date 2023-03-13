547de018abd12751b6c9a2e43d805536e9d42cb9
Prashant Deepake <deepakeprashant1@gmail.com>
2023-03-13 16:30:00 +0530

    Added: Passed Test Case 1.1 Records Match

--- src/main/java/bl/csvfile/example/StateCensusAnalyser.java
+++ src/main/java/bl/csvfile/example/StateCensusAnalyser.java
@@ -1,11 +1,13 @@
 package bl.csvfile.example;
 
 import com.opencsv.CSVWriter;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,8 +21,47 @@ public class StateCensusAnalyser {
 
     String filepath = "C:\\Users\\Admin\\IdeaProjects\\StateCensusAnalyser\\src\\main\\resources\\StateCensusData.csv";
 
-    public static int openCsvBuilder(String statecensusCsvfile, Class<CSVStateCensus> csvStateCensusClass) {
+    public static int openCsvBuilder(String csvFilePath, Object myClass) throws CensusAnalyserException {
+        int counter = 0;
+        try {
+            Iterator<Object> myIterator = getIterator(csvFilePath, myClass);
+            while ( myIterator.hasNext() ) {
+                counter++;
+                Object myObj = myIterator.next();
+            }
+        } catch (CensusAnalyserException e) {
+            throw e;
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.DELIMITER_ISSUE,
+                    "might be some error related to delimiter at record no. : " + (counter + 1));
+        }
+        return counter;
     }
+
+    public static Iterator<Object> getIterator(String csvFilePath, Object myClass) throws CensusAnalyserException {
+        Reader reader = null;
+        CsvToBean<Object> csvToBean;
+        try {
+            reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            csvToBean = new CsvToBeanBuilder(reader)
+                    .withType((Class) myClass)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<Object> myIterator = csvToBean.iterator();
+            return myIterator;
+        } catch (NoSuchFileException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE,
+                    "no such file exists. Please enter correct file");
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE,
+                    "delimiter error at line 1 OR might be some error " +
+                            "related to headers or incorrect extension / input file ");
+        } catch (IOException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.SOME_OTHER_IO_EXCEPTION,
+                    "Some other IO related exception");
+        }
+    }
+
 
     public void addStateCensusData(Scanner scan) {
         System.out.println("Enter State Name");
@@ -47,17 +88,17 @@ public class StateCensusAnalyser {
             CSVWriter writer = new CSVWriter(fileWriter);
             List<String[]> records = new ArrayList<String[]>();
             Iterator<CSVStateCensus> it = stateCensusesList.iterator();
-            while (it.hasNext()){
+            while (it.hasNext()) {
                 CSVStateCensus stateCensus1 = it.next();
                 records.add(new String[]{
-                   stateCensus1.getStateName(),
-                   stateCensus1.getPopulation(),
+                        stateCensus1.getStateName(),
+                        stateCensus1.getPopulation(),
                         stateCensus1.getAreaInSqKm()
                 });
             }
             writer.writeAll(records);
             writer.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
--- src/test/java/bl/census/StateCensusAnalyserTest.java
+++ src/test/java/bl/census/StateCensusAnalyserTest.java
@@ -1,4 +1,26 @@
 package bl.census;
-public class StateCensusAnalyserTest {
+import bl.csvfile.example.*;
+import bl.csvfile.example.CSVStateCensus;
+import bl.csvfile.example.CensusAnalyserException;
+import bl.csvfile.example.StateCensusAnalyser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+public class StateCensusAnalyserTest {
+    public static final String STATECODES_CSVFILE = "/home/admin97/Ayush/github/MyCensusAnalyser/src/main/resources/StateCode.csv";
+    public static final String STATECENSUS_CSVFILE = "C:\\Users\\Admin\\IdeaProjects\\StateCensusAnalyser\\src\\main\\resources\\StateCensusData.csv";
+    public static final String WRONG_FILE = "/useless.txt";
+
+    @Test
+    public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
+        try {
+            int count = StateCensusAnalyser.openCsvBuilder(STATECENSUS_CSVFILE, CSVStateCensus.class);
+            System.out.println(count);
+            Assertions.assertEquals(4,count);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
 }
